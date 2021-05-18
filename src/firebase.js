b38b77483356ab9e5807430c090414eaf1c232df
Zee <52600405+zeelib1@users.noreply.github.com>
2021-05-18 20:08:07 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,16 @@
-import firebase from 'firebase'
+// import firebase from 'firebase'
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-//   check the app on the hosted site; code is for demonstration purposes
+  apiKey: "AIzaSyAu4lbS5klRKnV2gfCTyTE7B-jD6FIqDfE",
+  authDomain: "clone-7c690.firebaseapp.com",
+  projectId: "clone-7c690",
+  storageBucket: "clone-7c690.appspot.com",
+  messagingSenderId: "859923958345",
+  appId: "1:859923958345:web:3ca628a29c2ea116cecd98",
+  measurementId: "G-Y9Y597YMSP"
 };
 //initialize the app
 const firebaseApp = firebase.initializeApp(firebaseConfig);
